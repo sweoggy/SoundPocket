776c0287248419d73da56512cd8b84cfa86337df
CarlTern <psy15cte@student.lu.se>
2019-05-07 17:25:10 +0200

    updated with timestamp

--- app/src/main/java/com/dynamicdusk/soundpocket/Mario.java
+++ app/src/main/java/com/dynamicdusk/soundpocket/Mario.java
@@ -1,9 +1,15 @@
 package com.dynamicdusk.soundpocket;
 
+import java.util.Calendar;
+
 public class Mario implements AccelerometerListener {
 
-
+    private long timeStamp = 0;
     private SoundPlayer soundPlayer;
+    public Mario(){
+        timeStamp = Calendar.getInstance().getTimeInMillis();
+    }
+
     public void setSoundPlayer(SoundPlayer soundPlayer){
         this.soundPlayer = soundPlayer;
     }
@@ -22,22 +28,25 @@ public class Mario implements AccelerometerListener {
     }
 
     public void onShakeX(float force) {
-        if(soundPlayer.isSoundOn()) {
+        if(soundPlayer.isSoundOn()&& (Calendar.getInstance().getTimeInMillis() - timeStamp) > 500) {
             soundPlayer.playSound(SoundPlayer.SOUND_FIREBALL);
+            timeStamp = Calendar.getInstance().getTimeInMillis();
         }
         //jsHandler.alert("Force: " + force);
     }
 
     public void onShakeY(float force) {
-        if(soundPlayer.isSoundOn()) {
+        if(soundPlayer.isSoundOn()&& (Calendar.getInstance().getTimeInMillis() - timeStamp) > 500) {
             soundPlayer.playSound(SoundPlayer.SOUND_COIN);
+            timeStamp = Calendar.getInstance().getTimeInMillis();
         }
         //jsHandler.alert("Force: " + force);
     }
 
     public void onShakeZ(float force) {
-        if(soundPlayer.isSoundOn()) {
+        if(soundPlayer.isSoundOn()&& (Calendar.getInstance().getTimeInMillis() - timeStamp) > 500) {
             soundPlayer.playSound(SoundPlayer.SOUND_PIPE);
+            timeStamp = Calendar.getInstance().getTimeInMillis();
         }
         //jsHandler.alert("Force: " + force);
     }
--- app/src/main/java/com/dynamicdusk/soundpocket/Warcraft3.java
+++ app/src/main/java/com/dynamicdusk/soundpocket/Warcraft3.java
@@ -2,11 +2,16 @@ package com.dynamicdusk.soundpocket;
 
 import android.content.Context;
 
+import java.util.Calendar;
+
 public class Warcraft3 implements AccelerometerListener {
 
+    private long timeStamp = 0;
     SoundPlayer soundPlayer;
-    
 
+    public Warcraft3(){
+        timeStamp = Calendar.getInstance().getTimeInMillis();
+    }
     public void setSoundPlayer(SoundPlayer soundPlayer){
         this.soundPlayer = soundPlayer;
     }
@@ -25,22 +30,25 @@ public class Warcraft3 implements AccelerometerListener {
     }
 
     public void onShakeX(float force) {
-        if(soundPlayer.isSoundOn()) {
+        if(soundPlayer.isSoundOn()&& (Calendar.getInstance().getTimeInMillis() - timeStamp) > 500) {
             soundPlayer.playSound(SoundPlayer.SOUND_WORK_WORK);
+            timeStamp = Calendar.getInstance().getTimeInMillis();
         }
         //jsHandler.alert("Force: " + force);
     }
 
     public void onShakeY(float force) {
-        if(soundPlayer.isSoundOn()) {
+        if(soundPlayer.isSoundOn()&& (Calendar.getInstance().getTimeInMillis() - timeStamp) > 500) {
             soundPlayer.playSound(SoundPlayer.SOUND_YES_MI_LORD);
+            timeStamp = Calendar.getInstance().getTimeInMillis();
         }
         //jsHandler.alert("Force: " + force);
     }
 
     public void onShakeZ(float force) {
-        if(soundPlayer.isSoundOn()) {
+        if(soundPlayer.isSoundOn()&& (Calendar.getInstance().getTimeInMillis() - timeStamp) > 500) {
             soundPlayer.playSound(SoundPlayer.SOUND_OFF_I_GO_THEN);
+            timeStamp = Calendar.getInstance().getTimeInMillis();
         }
         //jsHandler.alert("Force: " + force);
     }
