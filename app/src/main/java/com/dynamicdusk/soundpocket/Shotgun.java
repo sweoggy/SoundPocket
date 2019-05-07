013b481e42358d06a03651ac9cacafa0e7c2c7de
unknown <bas15jpe@student.lu.se>
2019-05-07 15:55:37 +0200

    Added delay in shotgun

--- app/src/main/java/com/dynamicdusk/soundpocket/Shotgun.java
+++ app/src/main/java/com/dynamicdusk/soundpocket/Shotgun.java
@@ -1,13 +1,22 @@
 package com.dynamicdusk.soundpocket;
 
+import java.sql.Timestamp;
+import java.util.Calendar;
+import java.util.Date;
+
 public class Shotgun implements AccelerometerListener {
 
     private int shots = 0;
     private boolean loaded;
     private boolean magazineCocked = false;
     private boolean magazinePulledForward;
+    private long timeStamp = 0;
     private SoundPlayer soundPlayer;
-    private int intervall;
+
+    public Shotgun(){
+        timeStamp = Calendar.getInstance().getTimeInMillis();
+    }
+
     public void setSoundPlayer(SoundPlayer soundPlayer){
         this.soundPlayer = soundPlayer;
     }
@@ -27,22 +36,25 @@ public class Shotgun implements AccelerometerListener {
 
     public void onShakeX(float force) {
 
-            if (soundPlayer.isSoundOn()) {
+            if (soundPlayer.isSoundOn()&& (Calendar.getInstance().getTimeInMillis() - timeStamp) > 500) {
                 if(magazineCocked &&shots>0) {
                 soundPlayer.playSound(SoundPlayer.SOUND_SHOTGUN_SHOT);
+                timeStamp = Calendar.getInstance().getTimeInMillis();
                 magazineCocked = false;
                 shots--;
             } else{
                     soundPlayer.playSound(SoundPlayer.SOUND_DRY_FIRE);
+                    timeStamp = Calendar.getInstance().getTimeInMillis();
                 }
         }
         //jsHandler.alert("Force: " + force);
     }
 
     public void onShakeY(float force) {
-        if (soundPlayer.isSoundOn()) {
+        if (soundPlayer.isSoundOn()&& (Calendar.getInstance().getTimeInMillis() - timeStamp) > 500) {
             if (shots > 0) {
                 soundPlayer.playSound(SoundPlayer.SOUND_SHOTGUN_RELOAD);
+                timeStamp = Calendar.getInstance().getTimeInMillis();
                 magazineCocked = true;
             }
             //jsHandler.alert("Force: " + force);
@@ -50,8 +62,9 @@ public class Shotgun implements AccelerometerListener {
     }
 
     public void onShakeZ(float force) {
-        if(soundPlayer.isSoundOn()) {
+        if(soundPlayer.isSoundOn() && (Calendar.getInstance().getTimeInMillis() - timeStamp) > 500) {
             soundPlayer.playSound(SoundPlayer.SOUND_AMMO_LOAD);
+            timeStamp = Calendar.getInstance().getTimeInMillis();
             if(shots<8) {
                 shots++;
             }
